c1816ccbfc645664a4b900d9497b5c3b60efed3d
kgmeister <43465569+kgmeister@users.noreply.github.com>
2021-03-11 16:14:59 +0800

    Update jiapalang.R
    fixed with vector

--- jiapalang.R
+++ jiapalang.R
@@ -1,13 +1,14 @@
 data <- c(1,2,3,4,5)
+vector <- c()
 
 test <- function(x){
-  container <- 0
   for(i in 1:length(x)){
-  if(x[i]<=3){
-        print (2*x[i])
-}
-else (print (x[i]))
+    if(x[i]<=3){
+      vector[i] <- x[i]*2
+    }
+    else vector[i] <- x[i]
   }
+  return(vector)
 }
 
 test(data)
